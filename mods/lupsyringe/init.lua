01a8a5f30a21b58a3f2f935e9689cf3fe4921155
Braxton Plaxco <bplaxco@lupnix.org>
2021-01-10 01:15:01 -0600

    :retab lupsyringe init.lua
    ~ B'ezrat Hashem ~

--- mods/lupsyringe/init.lua
+++ mods/lupsyringe/init.lua
@@ -1,37 +1,37 @@
 minetest.register_tool("lupsyringe:syringe_health", {
-	description = "Health Syringe",
-	inventory_image = "lupsyringe_health.png",
-	groups = {tool = 1},
+  description = "Health Syringe",
+  inventory_image = "lupsyringe_health.png",
+  groups = {tool = 1},
   on_use = minetest.item_eat(20, "lupsyringe:syringe_empty 1"),
 })
 
 minetest.register_craftitem("lupsyringe:syringe_empty", {
-	description = "Empty Syringe",
-	inventory_image = "lupsyringe_empty.png",
-	groups = {tool = 1},
+  description = "Empty Syringe",
+  inventory_image = "lupsyringe_empty.png",
+  groups = {tool = 1},
 })
 
 minetest.register_craft({
-	output = "lupsyringe:syringe_empty",
-	recipe = {
-		{"default:steel_ingot"},
-		{"default:glass"},
-	}
+  output = "lupsyringe:syringe_empty",
+  recipe = {
+    {"default:steel_ingot"},
+    {"default:glass"},
+  }
 })
 
 minetest.register_craft({
-	output = "lupsyringe:syringe_health",
-	recipe = {
-		{"default:steel_ingot"},
-		{"group:flora"},
-		{"default:glass"},
-	}
+  output = "lupsyringe:syringe_health",
+  recipe = {
+    {"default:steel_ingot"},
+    {"group:flora"},
+    {"default:glass"},
+  }
 })
 
 minetest.register_craft({
-	output = "lupsyringe:syringe_health",
-	recipe = {
-		{"group:flora"},
-		{"lupsyringe:syringe_empty"},
-	}
+  output = "lupsyringe:syringe_health",
+  recipe = {
+    {"group:flora"},
+    {"lupsyringe:syringe_empty"},
+  }
 })
