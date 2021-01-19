3861f1a0b668c636e26a6a14e2f2a2c7af4b3437
Braxton Plaxco <bplaxco@lupnix.org>
2021-01-19 01:44:56 -0600

    Make them explode
    ~ B'ezrat Hashem ~

--- mods/luphanukkah/init.lua
+++ mods/luphanukkah/init.lua
@@ -4,14 +4,21 @@ minetest.register_node("luphanukkah:dreidel_exploding", {
   description = "Just a harmless dreidel",
   visual = "mesh",
   tiles =  {"luphanukkah_dreidel.png"},
+  groups = {tnt = 1, dig_immediate = 2},
+  on_punch = function(pos, node, puncher)
+		if puncher:get_wielded_item():get_name() == "default:torch" then
+      tnt.burn(pos, node.name)
+    end
+  end,
 })
 
-minetest.register_node("luphanukkah:dreidel_exploding_lit", {
+minetest.register_node("luphanukkah:dreidel_exploding_burning", {
   drawtype = "mesh",
   mesh = "luphanukkah_dreidel.obj",
   visual = "mesh",
+  on_timer = function(pos, elapsed) tnt.boom(pos, { radius = 2 }) end,
   tiles =  {{
-    name = "luphanukkah_dreidel_lit_animated.png",
+    name = "luphanukkah_dreidel_burning_animated.png",
     animation = {type = "vertical_frames", aspect_w = 32, aspect_h = 32, length = 0.5},
   }},
 })
