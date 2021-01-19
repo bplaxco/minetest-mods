1dab6e44e297bfdfb9d180417aa552268acd35f7
Braxton Plaxco <bplaxco@lupnix.org>
2021-01-19 01:15:33 -0600

    Add dreidel and flashing dreidel
    ~ B'ezrat Hashem ~

--- mods/luphanukkah/init.lua
+++ mods/luphanukkah/init.lua
@@ -1,7 +1,19 @@
-tnt.register_tnt({
-  name = "luphanukkah:dreidel_exploding",
-  description = "Just a harmless dreidle",
-  raidus = 1,
+minetest.register_node("luphanukkah:dreidel_exploding", {
+  drawtype = "mesh",
+  mesh = "luphanukkah_dreidel.obj",
+  description = "Just a harmless dreidel",
+  visual = "mesh",
+  tiles =  {"luphanukkah_dreidel.png"},
+})
+
+minetest.register_node("luphanukkah:dreidel_exploding_lit", {
+  drawtype = "mesh",
+  mesh = "luphanukkah_dreidel.obj",
+  visual = "mesh",
+  tiles =  {{
+    name = "luphanukkah_dreidel_lit_animated.png",
+    animation = {type = "vertical_frames", aspect_w = 32, aspect_h = 32, length = 0.5},
+  }},
 })
 
 minetest.register_craft({
